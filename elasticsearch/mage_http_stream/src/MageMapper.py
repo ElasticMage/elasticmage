02e6506b0fff73f86f9414508f8ecad6f39d5c5c
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2013-10-27 11:54:06 +0000

    update category relations

--- elasticsearch/mage_http_stream/src/MageMapper.py
+++ elasticsearch/mage_http_stream/src/MageMapper.py
@@ -2,50 +2,46 @@
 
 class MageMapper(object):
 
-	def __init__(self, attributes = None):
-		self.attributes = attributes
+    def __init__(self, attributes = None):
+        self.attributes = attributes
 
-	def map(self, data):
-		if data["table"] == "catalog_category_product":
-			return self.__map_catagory_relations(data)
+    def map(self, data):
+        if data["table"] == "catalog_category_product":
+            return self.__map_catagory_relations(data)
 
-		if not data["table"].startswith("catalog_product_entity"):
-			return None
+        if not data["table"].startswith("catalog_product_entity"):
+            return None
 
-		try:
-			data["id"] = data["doc"]["entity_id"]
-		except:
-			pass
+        try:
+            data["id"] = data["doc"]["entity_id"]
+        except:
+            pass
 
-		if data["table"] != "catalog_product_entity":
-			return self.__map_eav_child_table(data)
+        if data["table"] != "catalog_product_entity":
+            return self.__map_eav_child_table(data)
 
-		return data
+        return data
 
-	def __map_eav_child_table(self, data):
-		try:
-			label = self.attributes.getLabel(data["doc"]["attribute_id"])
-			data["table"] = "catalog_product_entity"
-			data["action"] = "update"
-			doc = {}
-			doc["entity_id"] = data["doc"]["entity_id"]
-			doc[label] = data["doc"]["value"]
-			data["doc"] = doc
-			return data
-		except:
-			print "unsupported data", data
+    def __map_eav_child_table(self, data):
+        try:
+            label = self.attributes.getLabel(data["doc"]["attribute_id"])
+            data["table"] = "catalog_product_entity"
+            data["action"] = "update"
+            doc = {}
+            doc["entity_id"] = data["doc"]["entity_id"]
+            doc[label] = data["doc"]["value"]
+            data["doc"] = doc
+            return data
+        except:
+            print "unsupported data", data
 
-	def __map_catagory_relations(self, data):
-		ret = {}
-		ret["action"] = "update"
-		ret["table"] = "catalog_product_entity"
-		ret["id"] = data["doc"]["product_id"]
-		ret["doc"] = {
-			"categories": [
-				{ 
-					"id" : data["doc"]["category_id"],
-					"pos" : data["doc"]["position"]
-				}
-			]
-		}
-		return ret
+    def __map_catagory_relations(self, data):
+        ret = {}
+        ret["action"] = "update"
+        ret["table"] = "catalog_product_entity"
+        ret["id"] = data["doc"]["product_id"]
+        ret["doc"] = {
+            "categories": [ data["doc"]["category_id"] ],
+            "category_pos": { data["doc"]["category_id"] : data["doc"]["position"] }
+        }
+        return ret
--- elasticsearch/mage_http_stream/tests/MageMapper_test.py
+++ elasticsearch/mage_http_stream/tests/MageMapper_test.py
@@ -8,89 +8,88 @@ import fudge
 
 
 class MageMapper_(unittest.TestCase):
-	def test_if_it_skips_non_product_data(self):
-		eq_(
-			MageMapper().map({
-				"action" : "insert",
-				"table" : "core_config",
-				"doc" : {"sku" : "testSku"}
-			}),
-			None
-		)
+    def test_if_it_skips_non_product_data(self):
+        eq_(
+            MageMapper().map({
+                "action" : "insert",
+                "table" : "core_config",
+                "doc" : {"sku" : "testSku"}
+            }),
+            None
+        )
 
-	def test_if_it_keeps_document_for_the_main_product_table(self):
-		eq_(
-			MageMapper().map({
-				"action" : "insert",
-				"table" : "catalog_product_entity",
-				"doc" : {"sku" : "testSku"}
-			})["doc"],
-			{
-				"sku" : "testSku"
-			}
-		)
+    def test_if_it_keeps_document_for_the_main_product_table(self):
+        eq_(
+            MageMapper().map({
+                "action" : "insert",
+                "table" : "catalog_product_entity",
+                "doc" : {"sku" : "testSku"}
+            })["doc"],
+            {
+                "sku" : "testSku"
+            }
+        )
 
-			
-	def test_if_it_adds_item_id(self):
-		eq_(
-			MageMapper().map({
-				"action" : "insert",
-				"table" : "catalog_product_entity",
-				"doc" : {
-					"entity_id" : "12",
-					"sku" : "testSku"
-				}
-			})["id"],
-			"12"
-		)
+            
+    def test_if_it_adds_item_id(self):
+        eq_(
+            MageMapper().map({
+                "action" : "insert",
+                "table" : "catalog_product_entity",
+                "doc" : {
+                    "entity_id" : "12",
+                    "sku" : "testSku"
+                }
+            })["id"],
+            "12"
+        )
 
-	def test_if_it_converts_magento_eav_to_main_document_structure(self):
-		attributes = (fudge.Fake('MageAttributes')
-			.provides('getLabel')
-			.with_args('42')
-			.returns('my_attribute')
-		)
-		eq_(
-			MageMapper(attributes).map({
-				"action" : "insert",
-				"table" : "catalog_product_entity_int",
-				"doc" : {
-					"entity_id" : "12",
-					"entity_type_id" : "4242",
-					"attribute_id" : "42",
-					"value" : "9",
-				}
-			}),
-			{
-				"action" : "update",
-				"table" : "catalog_product_entity",
-				"id" : "12",
-				"doc" : {
-					"entity_id" : "12",
-					"my_attribute" : "9"
-				}
-			}
-		)
+    def test_if_it_converts_magento_eav_to_main_document_structure(self):
+        attributes = (fudge.Fake('MageAttributes')
+            .provides('getLabel')
+            .with_args('42')
+            .returns('my_attribute')
+        )
+        eq_(
+            MageMapper(attributes).map({
+                "action" : "insert",
+                "table" : "catalog_product_entity_int",
+                "doc" : {
+                    "entity_id" : "12",
+                    "entity_type_id" : "4242",
+                    "attribute_id" : "42",
+                    "value" : "9",
+                }
+            }),
+            {
+                "action" : "update",
+                "table" : "catalog_product_entity",
+                "id" : "12",
+                "doc" : {
+                    "entity_id" : "12",
+                    "my_attribute" : "9"
+                }
+            }
+        )
 
-	def test_if_it_reforms_category_relation(self):
-		eq_(
-			MageMapper().map({
-				"action" : "insert",
-				"table" : "catalog_category_product",
-				"doc" : {
-					"category_id" : "12",
-					"product_id" : "42",
-					"position" : "9"
-				}
-			}),
-			{
-				"action" : "update",
-				"table" : "catalog_product_entity",
-				"id" : "42",
-				"doc" : {
-					"categories" : [
-						{"id" : "12", "pos" : "9"}
-					]
-				}
-			}
-		)
+    def test_if_it_reforms_category_relation(self):
+        eq_(
+            MageMapper().map({
+                "action" : "insert",
+                "table" : "catalog_category_product",
+                "doc" : {
+                    "category_id" : "12",
+                    "product_id" : "42",
+                    "position" : "9"
+                }
+            }),
+            {
+                "action" : "update",
+                "table" : "catalog_product_entity",
+                "id" : "42",
+                "doc" : {
+                    "categories" : [ "12" ],
+                    "category_pos" : {"12" : "9"}
+                }
+            }
+        )
